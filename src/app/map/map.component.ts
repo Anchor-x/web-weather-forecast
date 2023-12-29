b7bcf71e09b558d38f42a0405920b70bd8ac5e32
fo1klore <870577519@qq.com>
2023-12-29 18:04:02 +0800

    添加所有省级行政单位js，可点击进入省级地图 （因为还没实现动态注册地图，所以初始化时直接注册全部地图，可能要等久一点（？））

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,9 +1,39 @@
 import {Component, OnInit} from '@angular/core';
 import { NgxEchartsModule } from 'ngx-echarts';
 import * as echarts from 'echarts';
-import * as Chongqing from "../../assets/js/Chongqing"
 import * as China from "../../assets/js/China"
+import * as Anhui from "../../assets/js/Anhui"
+import * as Beijing from "../../assets/js/Beijing"
+import * as Chongqing from "../../assets/js/Chongqing"
+import * as Fujian from "../../assets/js/Fujian"
+import * as Gansu from "../../assets/js/Gansu"
+import * as Guangdong from "../../assets/js/Guangdong"
+import * as Guangxi from "../../assets/js/Guangxi"
+import * as Guizhou from "../../assets/js/Guizhou"
+import * as Hainan from "../../assets/js/Hainan"
+import * as Hebei from "../../assets/js/Hebei"
+import * as Heilongjiang from "../../assets/js/Heilongjiang"
 import * as Henan from "../../assets/js/Henan"
+import * as Hubei from "../../assets/js/Hubei"
+import * as Hunan from "../../assets/js/Hunan"
+import * as Jiangsu from "../../assets/js/Jiangsu"
+import * as Jiangxi from "../../assets/js/Jiangxi"
+import * as Jilin from "../../assets/js/Jilin"
+import * as Liaoning from "../../assets/js/Liaoning"
+import * as Neimenggu from "../../assets/js/Neimenggu"
+import * as Ningxia from "../../assets/js/Ningxia"
+import * as Qinghai from "../../assets/js/Qinghai"
+import * as Shandong from "../../assets/js/Shandong"
+import * as Shanghai from "../../assets/js/Shanghai"
+import * as Shanx from "../../assets/js/Shanx"
+import * as Shanxi from "../../assets/js/Shanxi"
+import * as Sichuan from "../../assets/js/Sichuan"
+import * as Taiwan from "../../assets/js/Taiwan"
+import * as Tianjin from "../../assets/js/Tianjin"
+import * as Xinjiang from "../../assets/js/Xinjiang"
+import * as Xizang from "../../assets/js/Xizang"
+import * as Yunnan from "../../assets/js/Yunnan"
+import * as Zhejiang from "../../assets/js/Zhejiang"
 
 @Component({
   selector: 'app-map',
@@ -17,6 +47,39 @@ import * as Henan from "../../assets/js/Henan"
 export class MapComponent implements OnInit{
   ngOnInit() {
     China.init()
+    Beijing.init()
+    Tianjin.init()
+    Hebei.init()
+    Shanxi.init()
+    Neimenggu.init()
+    Liaoning.init()
+    Jilin.init()
+    Heilongjiang.init()
+    Shanghai.init()
+    Jiangsu.init()
+    Zhejiang.init()
+    Anhui.init()
+    Fujian.init()
+    Jiangxi.init()
+    Shandong.init()
+    Henan.init()
+    Hubei.init()
+    Hunan.init()
+    Guangdong.init()
+    Guangxi.init()
+    Hainan.init()
+    Chongqing.init()
+    Sichuan.init()
+    Guizhou.init()
+    Yunnan.init()
+    Xizang.init()
+    Shanx.init()
+    Gansu.init()
+    Qinghai.init()
+    Ningxia.init()
+    Xinjiang.init()
+    Fujian.init()
+    Taiwan.init()
     this.initChart();
   }
   initChart() {
@@ -52,65 +115,32 @@ export class MapComponent implements OnInit{
 
     myChart.on('click',function(param){
       console.log(param);
-      if(param.name=='重庆市') {
-
-        Chongqing.init();
-        myChart.setOption( {
-          title : {
-          },
-          tooltip: {},
-          legend: {
-          },
-          geo: {
-            map: param.name,
-            show: true,
-            roam:false,
-            zoom:1,
-            emphasis:{
-              label:{
-                show:false
-              }
-            },
-            itemStyle: {
-              areaColor: '#FFDAB9',
-              borderColor: '#FFE4E1',
-              shadowColor: '#FF6A6A',
-              shadowBlur: 10
+      myChart.setOption( {
+        title : {
+        },
+        tooltip: {},
+        legend: {
+        },
+        geo: {
+          map: param.name,
+          show: true,
+          roam:false,
+          zoom:1,
+          emphasis:{
+            label:{
+              show:false
             }
           },
-          series: [
-          ]
-        })
-      }
-      if(param.name=='河南省') {
-        Henan.init()
-        myChart.setOption( {
-          title : {
-          },
-          tooltip: {},
-          legend: {
-          },
-          geo: {
-            map: param.name,
-            show: true,
-            roam:false,
-            zoom:1,
-            emphasis:{
-              label:{
-                show:false
-              }
-            },
-            itemStyle: {
-              areaColor: '#FFDAB9',
-              borderColor: '#FFE4E1',
-              shadowColor: '#FF6A6A',
-              shadowBlur: 10
-            }
-          },
-          series: [
-          ]
-        })
-      }
+          itemStyle: {
+            areaColor: '#FFDAB9',
+            borderColor: '#FFE4E1',
+            shadowColor: '#FF6A6A',
+            shadowBlur: 10
+          }
+        },
+        series: [
+        ]
+      })
     })
     option && myChart.setOption(option);
   }

a959c25cf829460926fca3c5734eb2b7831d9a8a
fo1klore <870577519@qq.com>
2024-01-02 16:01:36 +0800

    block module added. index page completed. waiting for data to pass to the blocks.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MapComponent} from "./map/map.component";
+import {BlockComponent} from "./block/block.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [CommonModule, RouterOutlet, MapComponent],
+  imports: [CommonModule, RouterOutlet, MapComponent, BlockComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
